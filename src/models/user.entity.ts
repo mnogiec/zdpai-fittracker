29ab5ba72fc3490062875047947d82712857c781
mnogiec <nogiecmikolaj@gmail.com>
2024-12-23 08:51:58 +0100

    Create user entity

---
+++ src/models/user.entity.ts
@@ -0,0 +1,28 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column({ unique: true, select: false })
+  email: string;
+
+  @Column({ select: false })
+  password: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ default: false })
+  isAdmin: boolean;
+}
