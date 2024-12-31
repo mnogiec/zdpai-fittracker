daef391ce319e710acfe5a06a5d775a4e9264480
mnogiec <nogiecmikolaj@gmail.com>
2024-12-31 12:10:58 +0100

    Make health check public

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import { PublicRoute } from './auth/decorators/publicRoute.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/health')
+  @PublicRoute()
   checkHealth(): string {
     return this.appService.checkHealth();
   }
